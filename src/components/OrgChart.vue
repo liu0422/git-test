d41e415e08781001639ea77e48214e2513113788
lc <1909476877@qq.com>
2024-12-06 11:11:43 +0800

    init

---
+++ src/components/OrgChart.vue
@@ -0,0 +1,31 @@
+<div class="org-chart">
+  <ul>
+    <li>
+      <div class="node">某某科技有限公司</div>
+      <ul>
+        <li>
+          <div class="node">研发部</div>
+          <ul>
+            <li><div class="node">前端组</div></li>
+            <li><div class="node">后端组</div></li>
+            <li><div class="node">测试组</div></li>
+          </ul>
+        </li>
+        <li>
+          <div class="node">市场部</div>
+          <ul>
+            <li><div class="node">销售组</div></li>
+            <li><div class="node">市场策划组</div></li>
+          </ul>
+        </li>
+        <li>
+          <div class="node">人力资源部</div>
+          <ul>
+            <li><div class="node">招聘组</div></li>
+            <li><div class="node">培训组</div></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</div>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <org-chart :data="orgData" />
+  </div>
+</template>
+
+<script>
+import OrgChart from '@/components/OrgChart.vue'
+
+export default {
+  name: 'Example',
+  components: {
+    OrgChart
+  },
+  data() {
+    return {
+      orgData: {
+        name: '某某科技有限公司',
+        children: [
+          {
+            name: '研发部',
+            children: [
+              { name: '前端组' },
+              { name: '后端组' },
+              { name: '测试组' }
+            ]
+          },
+          {
+            name: '市场部',
+            children: [
+              { name: '销售组' },
+              { name: '市场策划组' }
+            ]
+          },
+          {
+            name: '人力资源部',
+            children: [
+              { name: '招聘组' },
+              { name: '培训组' }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
