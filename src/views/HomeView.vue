d185370a0a2f8af2cfb040eab7fbc1defda12b40
lc <1909476877@qq.com>
2024-12-25 10:02:27 +0800

    防抖ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>home</div>
+  <p>
+    This text only updates 1 second after you've stopped typing:
+  </p>
+  <p>{{ text }}</p>
+  <input v-model="text" />
 </template>
-
-<script setup>
-
+<!-- <script setup>
+import { useDebouncedRef } from '@/utils/debouncedRef'
+const text = useDebouncedRef('hello', 1000)
+</script> -->
+<script>
+import { useDebouncedRef } from '@/utils/debouncedRef'
+export default {
+  data() {
+    return {
+      text: ''
+    }
+  },
+  mounted() {
+    this.text = useDebouncedRef('hello', 1000)
+  }
+}
 </script>
-
-<style scoped></style>
